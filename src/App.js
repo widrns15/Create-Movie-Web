4c002966e65975e2c8976497aa4a9fba4a779b8f
widrns15 <widrns15@naver.com>
2023-04-01 00:06:36 +0900

    🥽 test: map 함수를 이용하여 영화 리스트 만들어보기

--- src/App.js
+++ src/App.js
@@ -2,68 +2,41 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [get, setGet] = useState("");
-  const [cost, setCost] = useState(1);
-
-  const onChange = (event) => {
-    setCost(event.target.value);
-    setGet(1);
-  };
-
-  const handleInput = (event) => {
-    setGet(event.target.value);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
   };
 
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
+    getMovies();
   }, []);
 
   return (
     <div>
-      <h1>💰 The coins! {loading ? "" : `(${coins.length})`}</h1>
       {loading ? (
-        <strong>loading...</strong>
+        <h1>Loading...</h1>
       ) : (
-        <select onChange={onChange}>
-          <option>👉 Select coins!</option>
-          {coins.map((coin, index) => (
-            <option key={index} value={coin.quotes.USD.price}>
-              💵 {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
-            </option>
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} alt={movie.title} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
           ))}
-        </select>
+        </div>
       )}
-      <br />
-      <br />
-      <br />
-      <hr />
-      <br />
-      <h2>Please enter the amount ⬇️</h2>
-
-      <input
-        onChange={handleInput}
-        value={get}
-        type="number"
-        placeholder="dollor"
-        autoFocus
-        required
-      />
-      <br />
-      <br />
-      <br />
-      <hr />
-      <br />
-      <h1>
-        You can buy <br />
-        <br />
-        💵 ({get / cost}) coins
-      </h1>
     </div>
   );
 }
