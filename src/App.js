f2028639a39d37d18f6f44ecbe86ce49af8b606f
widrns15 <widrns15@naver.com>
2023-03-31 15:51:44 +0900

    🥽 test: map 함수를 이용하여 Coin Tracker 거래소 만들어보기

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ import { useEffect, useState } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [get, setGet] = useState("");
+  const [cost, setCost] = useState(1);
+
+  const onChange = (event) => {
+    setCost(event.target.value);
+    setGet(1);
+  };
+
+  const handleInput = (event) => {
+    setGet(event.target.value);
+  };
 
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
@@ -16,14 +27,43 @@ function App() {
   return (
     <div>
       <h1>💰 The coins! {loading ? "" : `(${coins.length})`}</h1>
-      {loading ? <strong>loading...</strong> : null}
-      <ul>
-        {coins.map((coin) => (
-          <li>
-            {coin.name} ({coin.symbol}): {coin.quotes.USD.price}
-          </li>
-        ))}
-      </ul>
+      {loading ? (
+        <strong>loading...</strong>
+      ) : (
+        <select onChange={onChange}>
+          <option>👉 Select coins!</option>
+          {coins.map((coin, index) => (
+            <option key={index} value={coin.quotes.USD.price}>
+              💵 {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
+      <br />
+      <br />
+      <br />
+      <hr />
+      <br />
+      <h2>Please enter the amount ⬇️</h2>
+
+      <input
+        onChange={handleInput}
+        value={get}
+        type="number"
+        placeholder="dollor"
+        autoFocus
+        required
+      />
+      <br />
+      <br />
+      <br />
+      <hr />
+      <br />
+      <h1>
+        You can buy <br />
+        <br />
+        💵 ({get / cost}) coins
+      </h1>
     </div>
   );
 }
