6b48385ff8673b0e38fe44a70ea2d6fa86494612
widrns15 <widrns15@naver.com>
2023-04-04 00:54:36 +0900

    🔥 feat: 홈페이지에서 영화 제목 클릭 시, 상세 페이지로 이동 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
   return (
     <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/movie/:id" element={<Detail />} />
       </Routes>
     </HashRouter>
   );
