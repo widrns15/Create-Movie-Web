fc54c44c741963c251194706e9cc5d3677a2b393
widrns15 <widrns15@naver.com>
2023-04-05 18:54:37 +0900

    🔥 feat: 각 영화에 맞는 상세 페이지 구현 및 간단한 디자인

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,22 +1,46 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
 
 function Detail() {
   const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState([]);
 
-  const getMovies = async () => {
+  const getMovie = useCallback(async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-    console.log(json);
-  };
-
+    setMovie(json.data.movie);
+    setLoading(false);
+  }, [id]);
   useEffect(() => {
-    getMovies();
-  }, []);
+    getMovie();
+  }, [getMovie]);
 
-  return <h1>Detail</h1>;
+  return (
+    <MainSection>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          <img src={movie.large_cover_image} alt="poster" />
+          <p>
+            🎬 Title: {movie.title}, {movie.year}
+          </p>
+          <p>✅ Download: {movie.download_count}</p>
+        </div>
+      )}
+    </MainSection>
+  );
 }
+
+const MainSection = styled.div`
+  position: relative;
+  text-align: center;
+  h1 {
+    margin-top: 360px;
+  }
+`;
 
 export default Detail;
